065e1c578afd9ad0f09770cb8ac07a7c1e5fd435
blacksmithalex <blacksmithalex@mail.ru>
2022-08-28 13:41:03 +0300

    new tasks

--- Tinkoff Generation 2022/6/code.py
+++ Tinkoff Generation 2022/6/code.py
@@ -9,10 +9,10 @@ else:
     if m >= 3:
         P2 = 2 * m + 6 * n
     if m >= 2:
-        P3min = 4 * n + 2 * m + 2 * (m // 2)
+        P3min = 4 * n + 2 * m + 2
         P3max = 4 * n + 2 * m + 2 * (m - 1)
     if n >= 2:
-        P4min = 4 * m + 2 * n + 2 * (n // 2)
+        P4min = 4 * m + 2 * n + 2
         P4max = 4 * m + 2 * n + 2 * (n - 1)
     if c == P1 or c == P2 or P3min <= c <= P3max or P4min <= c <= P4max:
         print('YES')
